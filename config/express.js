3b8d9caf432715f5ed832775cfb0cae3280a670a
James Cryer <info@jamescryer.com>
2014-04-30 00:58:08 +0100

    Init commit

---
+++ config/express.js
@@ -0,0 +1,74 @@
+/**
+ * Module dependencies.
+ */
+var express        = require('express'),
+    consolidate    = require('consolidate'),
+    session        = require('express-session'),
+    morgan         = require('morgan'),
+    cookieParser   = require('cookie-parser'),
+    serveStatic    = require('serve-static'),
+    bodyParser     = require('body-parser'),
+    compression    = require('compression'),
+    methodOverride = require('method-override'),
+    //MongoStore     = require('connect-mongo')({session: session}),
+    flash          = require('connect-flash'),
+    config         = require('./config');
+
+module.exports = function(app, db) {
+    app.set('showStackError', true);
+
+    //Should be placed before express
+    app.use(compression({
+        filter: function(req, res) {
+            return (/json|text/).test(res.getHeader('Content-Type'));
+        },
+        level: 9
+    }));
+
+    //Don't use logger for test env
+    if (process.env.NODE_ENV === 'development') {
+        app.use(morgan('dev'));
+    }
+
+    // Enable jsonp
+    app.enable('jsonp callback');
+
+    // The cookieParser should be above session
+    app.use(cookieParser());
+
+    // Request body parsing middleware should be above methodOverride
+    app.use(bodyParser.urlencoded());
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+
+    // Express/Mongo session storage
+    // app.use(session({
+    //     secret: config.sessionSecret,
+    //     store: new MongoStore({
+    //         db: db.connection.db,
+    //         collection: config.sessionCollection
+    //     })
+    // }));
+
+    // Connect flash for flash messages
+    app.use(flash());
+
+    // Setting the fav icon and static folder
+    app.use(serveStatic(config.root + '/public'));
+
+    // Assume "not found" in the error msgs is a 404. this is somewhat
+    // silly, but valid, you can do whatever you like, set properties,
+    // use instanceof etc.
+    app.use(function(err, req, res, next) {
+        // Treat as 404
+        if (~err.message.indexOf('not found')) return next();
+
+        // Log it
+        console.error(err.stack);
+
+        // Error page
+        res.status(500).render('500', {
+            error: err.stack
+        });
+    });
+};
---
+++ gruntfile.js
@@ -0,0 +1,71 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    // show elapsed time at the end
+    require('time-grunt')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+    // Project Configuration
+    var yeomanConfig = {
+        app: 'app',
+        pub: 'public',
+        dist: 'dist'
+    };
+
+    grunt.initConfig({
+        yeoman: yeomanConfig,
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            all: {
+                src: ['gruntfile.js', 'test/**/*.js', 'app/controllers/**/*.js'],
+                options: {
+                    jshintrc: true
+                }
+            }
+        },
+        nodemon: {
+            dev: {
+                script: 'app.js',
+                options: {
+                    args: [],
+                    ignoredFiles: ['README.md', 'node_modules/**', '.DS_Store'],
+                    watchedExtensions: ['js'],
+                    watchedFolders: ['app', 'config'],
+                    debug: true,
+                    delayTime: 1,
+                    env: {
+                        PORT: 3000
+                    },
+                    cwd: __dirname
+                }
+            }
+        },
+
+        concurrent: {
+            tasks: ['nodemon'],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
+        mochaTest: {
+            options: {
+                reporter: 'spec'
+            },
+            src: ['test/**/*.js']
+        }
+    });
+
+    //Making grunt default to force in order not to break the project.
+    grunt.option('force', true);
+
+    //Default task(s).
+    grunt.registerTask('default', ['jshint', 'concurrent']);
+
+    // Default task(s).
+    grunt.registerTask('build', ['jshint']);
+
+    //Test task.
+    grunt.registerTask('test', ['mochaTest']);
+};
