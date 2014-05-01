7d11cb4d571d1385204243ef6354d3a8dd5e3286
James Cryer <info@jamescryer.com>
2014-05-01 11:42:29 +0100

    Modify loading configuration

--- config/config.js
+++ config/config.js
@@ -5,9 +5,7 @@ var _ = require('underscore'),
 
 // Load configurations
 // Set the node environment variable if not set before
-process.env.NODE_ENV = ~fs.readdirSync('./config/env').map(function(file) {
-    return file.slice(0, -3);
-}).indexOf(process.env.NODE_ENV) ? process.env.NODE_ENV : 'development';
+process.env.NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
 
 // Extend the base configuration in all.js with environment
 // specific configuration
